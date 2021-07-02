3cde6a051fa451d361bff78f7409bc462ff6a9e1
Chris MacLellan <2348-cm3786@users.noreply.gitlab.cci.drexel.edu>
2021-07-02 18:24:01 -0400

    removed unnecessary import

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import uuid
 from flask import Flask
 from flask import render_template
 from flask import redirect
-from flask import request
 from flask import session
 from flask import send_from_directory
 
